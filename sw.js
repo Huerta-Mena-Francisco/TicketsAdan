e922eb5babf44db7242a20d7204db96c8d4c6efc
Huerta-Mena-Francisco <javihuerta69921@gmail.com>
2026-06-10 10:20:31 -0600

    Eliminar logo y actualizar a TickesSol

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'chicho-tickets-v1';
+const CACHE_NAME = 'tickessol-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './Logo.jpeg'
+  '/',
+  '/Index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -10,11 +12,42 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
